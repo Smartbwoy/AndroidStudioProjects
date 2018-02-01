dca914572b89f77842343f950b44ec85a6e80a0d
Miguel Jackson <rrennie66@gmail.com>
2018-02-01 13:42:20 -0500

    update

--- MapTest/app/src/main/java/com/example/maptest/maptest/MapsActivity.java
+++ MapTest/app/src/main/java/com/example/maptest/maptest/MapsActivity.java
@@ -1,7 +1,7 @@
-package com.example.smartbwoy.cookitrite;
+package com.example.maptest.maptest;
 
-import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,15 +10,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ResturantsFragment extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_resturants);
-
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
