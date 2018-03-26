a7b1b9bc1bdca78ff0c7bb184a6ebd796f029b23
Miguel Jackson <rrennie66@gmail.com>
2018-03-25 21:39:38 -0500

    Menu bar

--- TheServiceApp/app/src/main/java/com/advancemoms/theserviceapp/display_workers_industry.java
+++ TheServiceApp/app/src/main/java/com/advancemoms/theserviceapp/display_workers_industry.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -88,5 +90,28 @@ public class display_workers_industry extends AppCompatActivity {
             workersRatings.setRating(rating[i]);
             return view;
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            //startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- TheServiceApp/app/src/main/java/com/advancemoms/theserviceapp/userHomeScreen.java
+++ TheServiceApp/app/src/main/java/com/advancemoms/theserviceapp/userHomeScreen.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -158,6 +160,28 @@ public class userHomeScreen extends AppCompatActivity {
     static int getWorkerNum (){
         return wNumb;
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            //startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
 
